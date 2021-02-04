e59b3ae90fb5f7040fbdd316e2fda5a47bfe4d09
George Dilthey <george.dilthey@gmail.com>
2021-02-04 14:48:13 -0500

    Reorganized files

---
+++ js/content.js
@@ -0,0 +1,14 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.message === "clicked_browser_action") {
+            function injectScript(file, node) {
+                var th = document.getElementsByTagName(node)[0];
+                var s = document.createElement('script');
+                s.setAttribute('type', 'text/javascript');
+                s.setAttribute('src', file);
+                th.appendChild(s);
+            }
+            injectScript(chrome.extension.getURL('/js/inject.js'), 'body');
+        }
+    }
+);
