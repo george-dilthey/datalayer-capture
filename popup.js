463ecd4e215c770babd72b3f77e44f640c20cfc3
George Dilthey <george.dilthey@gmail.com>
2021-02-04 13:17:12 -0500

    Began development on appending child

--- popup.js
+++ popup.js
@@ -4,17 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
   
       chrome.tabs.getSelected(null, function(tab) {
         d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
+        var node = document.createElement("p");
+        var textnode = document.createTextNode("Testing!");
+        node.appendChild(textnode);
+        d.body.appendChild(node);
       });
     }, false);
   }, false);
