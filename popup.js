b3697aa8c442fe83d047c2bf4d07e7b11c110c4e
George Dilthey <george.dilthey@gmail.com>
2021-02-04 13:59:44 -0500

    Added background.js

--- popup.js
+++ popup.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
         d = document;
         var node = document.createElement("p");
-        var textnode = document.createTextNode("Testing!");
-        node.appendChild(textnode);
-        d.body.appendChild(node);
-      });
+        var dl = window.dataLayer;
+        if(dl != null && dl != undefined){
+            console.log(dl);
+            console.log('hi');
+            var textnode = document.createTextNode(dl);
+            node.appendChild(textnode);
+            d.body.appendChild(node);
+        }
+        console.log('hello')
     }, false);
   }, false);
